f19ac1cc938a9d4f06425602c972df9982393da6
Khomotso <110098964+Justhadtobe@users.noreply.github.com>
2023-11-12 14:59:15 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+function print() {
+    window.print();
+}
+var typed = new typed(".text", {
+    strings: ["Software Developer","Programmer","Freelancer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop:true
+})
